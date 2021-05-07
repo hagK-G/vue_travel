331f0443fa301acf2adbe16f56103e1ef0879438
hagK-G <jie806181451@163.com>
2021-05-08 04:20:22 +0800

    index-recommend

---
+++ travel/src/pages/components/Recommend.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item in recommendList" :key="item.id">
+        <img class="item-img" :src="item.url" />
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'recommend',
+    data() {
+      return {
+        recommendList: [{
+            'id': '001',
+            'url': 'http://img1.qunarzz.com/sight/p0/1804/c8/c88bb5e69c9a9c5ea3.img.jpg_200x200_740720ff.jpg',
+            'title': '古北水镇',
+            'desc': '复古乳房很快就恢复会计师费好'
+          },
+          {
+            'id': '002',
+            'url': 'http://img1.qunarzz.com/sight/p0/1804/c8/c88bb5e69c9a9c5ea3.img.jpg_200x200_740720ff.jpg',
+            'title': '古北水镇',
+            'desc': '复古乳房很快就恢复会计师费好'
+          },
+          {
+            'id': '003',
+            'url': 'http://img1.qunarzz.com/sight/p0/1804/c8/c88bb5e69c9a9c5ea3.img.jpg_200x200_740720ff.jpg',
+            'title': '古北水镇',
+            'desc': '复古乳房很快就恢复会计师费好'
+          },
+          {
+            'id': '004',
+            'url': 'http://img1.qunarzz.com/sight/p0/1804/c8/c88bb5e69c9a9c5ea3.img.jpg_200x200_740720ff.jpg',
+            'title': '古北水镇',
+            'desc': '复古乳房很快就恢复会计师费好'
+          },
+          {
+            'id': '005',
+            'url': 'http://img1.qunarzz.com/sight/p0/1804/c8/c88bb5e69c9a9c5ea3.img.jpg_200x200_740720ff.jpg',
+            'title': '古北水镇',
+            'desc': '复古乳房很快就恢复会计师费好'
+          },
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        color: #fff
+</style>
+
